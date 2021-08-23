2aae7cf21b88819540d2166167f5fee49d25c891
Charlie Weber <charlestweber@gmail.com>
2021-08-23 13:51:41 -0700

    initial commit, with preliminary files and some js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic for Passport
+// function Passport() {
+//   this.destinationss = {};
+//   this.currentId = 0;
+// }
+// Passport.prototype.addDestination = function (destination){
+//   destination.id = this.assignId();
+
+// }
+
+
+
+
+
+//Business logic for country
+
+function Destination(city, landmarks, season, notes){
+this.city = city
+this.landmarks = landmarks
+this.season = season
+this.notes = notes
+}  
