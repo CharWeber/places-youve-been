da1df9048a1e86a05d249f3968b32020d77e4a06
Charlie Weber <charlestweber@gmail.com>
2021-08-23 14:39:09 -0700

    Passed both passport function, destination function, add destination, find and delete destination

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
 //Business Logic for Passport
-// function Passport() {
-//   this.destinationss = {};
-//   this.currentId = 0;
-// }
-// Passport.prototype.addDestination = function (destination){
-//   destination.id = this.assignId();
+function Passport() {
+  this.destinations = {};
+  this.currentId = 0;
+}
 
-// }
-
-
-
+Passport.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
+  this.destinations[destination.id] = destination;
+};
+Passport.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+Passport.prototype.findDestination = function(id) {
+  if (this.destinations[id] != undefined) {
+    return this.destinations[id];
+  }
+  return false;
+};
+Passport.prototype.deleteDestination = function(id) {
+  if (this.destinations[id] === undefined) {
+    return false;
+  }
+  delete this.destinations[id];
+  return true;
+};
 
 
 //Business logic for country
 
-function Destination(city, landmarks, season, notes){
-this.city = city
+function Destination(country, city, landmarks, season, notes){
+this.country = country
+  this.cities = city
 this.landmarks = landmarks
 this.season = season
 this.notes = notes
 }  
+
+// Destination.prototype.addCity(city) {
+//   this.cities.push(city);
+// };
